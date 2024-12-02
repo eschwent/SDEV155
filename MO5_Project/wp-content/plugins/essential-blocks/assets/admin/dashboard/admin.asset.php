d0a274de7ae6ac4a9f10fca844340fd3bb9b2716
Emily Schwent <emilyschwent@MacBookAir.lan>
2024-12-02 01:49:55 -0600

    Initial Commit

---
+++ MO5_Project/wp-content/plugins/essential-blocks/assets/admin/dashboard/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '519e9d2e98f67a4337d4');
---
+++ MO5_Project/wp-content/plugins/essential-blocks/assets/vendors/js/bundles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'ff902812c8325e8c1d41');
